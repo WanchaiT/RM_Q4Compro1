bd7d79c85e89c5352d7dc36d4d7c45405826508f
Wanchai Thiantanawat <wanchai.thiantanawat@hotmail.com>
2019-01-24 22:27:21 +0700

    soled number_occ

--- number_occurence/number_occurence.java
+++ number_occurence/number_occurence.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 class number_occurence{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         int num = scan.nextInt();
         int[] dataNum = new int[num];
 
@@ -9,11 +10,16 @@ class number_occurence{
             dataNum[i] = scan.nextInt();
         }
         int target = scan.nextInt();
+        boolean check = false;
 
         for (int i = 0;i < num ;i++ ) {
             if (dataNum[i] == target) {
-                System.out.println(i+1);
+                System.out.print(i+1 + " ");
+                check = true;
             }
+        }
+        if (!check) {
+            System.out.println("0");
         }
     }
 }
