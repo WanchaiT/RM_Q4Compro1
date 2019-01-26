3d52887f4fd2a7e157d05863c610ccaef7b95164
Wanchai Thiantanawat <wanchai.thiantanawat@hotmail.com>
2019-01-26 11:35:34 +0700

    RPSSet

--- RockPapScis/RockPapScis.java
+++ RockPapScis/RockPapScis.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+class RockPapScis {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
 
+        if (a == b) {
+            System.out.println("0");
+        }else if (a == 1 && b == 3) {
+            System.out.println("1");
+        }else if (a == 3 && b == 1) {
+            System.out.println("2");
+        }else if (a > b) {
+            System.out.println("1");
+        }else {
+            System.out.println("2");
+        }
+    }
+}
